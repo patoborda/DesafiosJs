60f02a5e599aaae0586f5624430d0a6d11c3321b
borda_patricio <patricio.borda@outlook.com>
2022-09-27 09:52:32 -0300

    Proyecto finalizado

--- desafio3/Js/app.js
+++ desafio3/Js/app.js
@@ -16,7 +16,7 @@ class Producto {
 function sumaIva(precio) {
     return precio * 1.21;
 }
-
+// Variables
 let carga;
 let bandera = false;
 let productoDescuento = 0;
@@ -28,7 +28,7 @@ let nombreProducto = form.children[1].value;
 let precioProducto = form.children[3].value;
 let cantidadProducto = form.children[5].value;
 
-
+// Validador de datos, asi evitamos que el usuario ponga en blanco alguna casilla
 function validarDatos() {
     nombreProducto = form.children[1].value;
     precioProducto = form.children[3].value;
@@ -48,8 +48,10 @@ function validarDatos() {
         bandera = true;
     }
 }
+//Aplicacion de funcion flecha
+//Funcion de mostrar lo que se agrego al carrito + el uso del Toastify
 const mostrarProd = () => {
-    // ****************************************** Uso de libreria Toastify
+    // ***** Uso de libreria Toastify
     Toastify({
         text: `Producto agregado x ${cantidadProducto} uds`,
         duration: 3000,
@@ -65,12 +67,12 @@ const mostrarProd = () => {
             <li>El total con IVA es de: ${(precioProducto*cantidadProducto*1.21)}</li>`;
 }
 
-
+//Funcion para agregar producto a carrito/almacen de stock
 function agregarProd(e) {
     e.preventDefault()
     validarDatos();
     if (bandera == true) {
-        swal ({
+        swal ({          //Este swal funciona como un confirm, utilizando el then.
             text: "Desea agregar producto a carrito?",
             icon: "info"
         }).then(res => {
@@ -88,7 +90,7 @@ function agregarProd(e) {
             }})  
     }else {
         //alert("No se agrego producto a carrito")
-        //******************************************* Uso libreria SweetAlert
+        //********** Uso libreria SweetAlert
         swal({
             title: "No se agrego producto",
             timer: "3000",
@@ -98,49 +100,49 @@ function agregarProd(e) {
     }
 }
 
+//Funcion de filtrado por x cantidad de producto, aplicacion del metodo filter, para un producto que supere || iguale las 10 unidades
 const filtroMayor = () => {
-      
       let ventaMayor = carrito.filter(producto => producto.cantidad >= 10);
       if (ventaMayor.length > 0) {
           mostrarCarrito1.innerHTML += "<h3>Venta por mayor de los siguientes productos</h3>";
           for (const producto of ventaMayor) {
               mostrarCarrito1.innerHTML +=
-                  `<p>Producto: ${producto.nombre}</p>
+                  `<h5>Producto: ${producto.nombre}</h5>
              <p>Cantidad: ${producto.cantidad}</p>
              <p>Total de descuento por mayor: ${(producto.precio*1.21*0.1) * producto.cantidad}</p>`
               productoDescuento += (producto.precio*1.21*0.1) * producto.cantidad
           }
       }
 }
-
+//Funcion para mostrar carrito
 const mostrarCarrito = (e) => {
-    //aplique un OR ya que si en el localstorage no tengo un valor, este me va a dar indefinido por lo cual el OR me a elegir el array [] ********************************************************************
+    //aplique un OR ya que si en el localstorage no tengo un valor, este me va a dar indefinido por lo cual el OR me va elegir el array [] *************
     carrito = JSON.parse(localStorage.getItem("carrito")) || [];
     productoDescuento = 0;
     e.preventDefault();
     mostrarCarrito1.innerHTML = "<h3>Carrito</h3>";
     if(carrito.length>0){
-        
+    //Recorrido del array de productos del carrito con el for of, para luego hacer print en el html    
     for (const producto of carrito) {
         mostrarCarrito1.innerHTML += `
-            <p>Producto: ${producto.nombre}</p>
+            <h5>Producto: ${producto.nombre}</h5>
             <p>Precio: ${producto.precio}</p>
             <p>Cantidad: ${producto.cantidad}</p>
             <p>Total del producto con IVA: ${producto.cantidad*producto.precio*1.21}</p>`;
     }
         filtroMayor()
-        const precioCarrito = carrito.map(elemto => elemto.precio * 1.21 * elemto.cantidad);
-        let precioTotal = precioCarrito.reduce((a, b) => a + b);
-        precioTotal -= productoDescuento;
+        const precioCarrito = carrito.map(elemto => elemto.precio * 1.21 * elemto.cantidad);     //Utilizacion del map para crear un nuevo array solamente con los precio de los productos por unidad multiplicado por le Iva y por la cantidad seleccionada de ese producto
+        let precioTotal = precioCarrito.reduce((a, b) => a + b);              // Utilizacion del reduce para sumar todos los numeros del nuevo array, para obtener el total.
+        precioTotal -= productoDescuento;       //Precio total - el descuento si hay productos por mayor
         mostrarCarrito1.innerHTML +=
-            `<h4>El total de carrito es de: ${precioTotal}</h4>`;
+            `<h4>El total de carrito es de: ${precioTotal}$</h4>`;
     }else {
         mostrarCarrito1.innerHTML +=
             `<h4>No hay productos en el carrito</h4>`;
     }
 }
 
-
+// Eventos
 form.addEventListener("submit", agregarProd);
 btnMostrar.addEventListener("click", mostrarCarrito)
 
--- desafio3/Js/inicio.js
+++ desafio3/Js/inicio.js
@@ -3,12 +3,12 @@
 let formulario = document.getElementById("login")
 let registro = document.getElementById("registro")
 let resultado = document.getElementById("error");
-
+//Alerta de bienvenida!
 swal({
     title:"Bienvenido",
     timer: "3000",
 })
-
+// Constructor de usuario
     class Usuario {
         constructor(user, pass) {
             this.user = user;
@@ -16,15 +16,17 @@ swal({
         }
     }
 
-//"base de datos"
-let usuarios = [];
-let usuario1 = new Usuario("PATO", "123");
-let usuario2 = new Usuario("LUCIO", "321");
-let usuario3 = new Usuario("LEANDRO", "111");
-usuarios.push(usuario1, usuario2, usuario3);
-
-// let usuariosAlmacenados = JSON.stringify(usuarios);
-// localStorage.setItem("usuarios", usuariosAlmacenados);
+    // Array de usuarios donde voy a cargar los registrados
+    let usuarios  = [];
+    
+    //************************************ "base de datos" ************************************
+    // Esto simula una base de datos ya precargada
+    
+    let usuario1 = new Usuario("PATO", "123");
+    let usuario2 = new Usuario("LUCIO", "321");
+    let usuario3 = new Usuario("LEANDRO", "111");
+    usuarios.push(usuario1, usuario2, usuario3);
+   
 
 
 
@@ -36,11 +38,16 @@ formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     let userLogin = document.getElementById("user").value;
     let passLogin = document.getElementById("pass").value;
+    //Carga de base de datos al local storage. Si el ls esta vacio y queremos logear con x usuario sin registrar da null por ende va a tomar la "base de datos" ya predefinida  y lo va a subir al ls
+    //Esto nos evita que nos de un error de null al utilizar el metodo find en un array vacio. A su vez evitamos que se pise la "base de datos" con los usuarios registrados.
+    usuarios = JSON.parse(localStorage.getItem("usuarios")) || usuarios;
+    let usuariosAlmacenados = JSON.stringify(usuarios);
+    localStorage.setItem("usuarios", usuariosAlmacenados);
 
-    usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    // Prueba operadores ternarios
     const permiso = (userLogin == "") ? true : false
     permiso ? console.log("No puso usuario") : console.log("Si puso usuario")
-                
+         // Utilizacion del metodo find, en vez de usar un for of, con el find es mucho mas eficiente       
         let usuarioEncontrado = usuarios.find(user => user.user === userLogin && user.pass === passLogin);
             const bandera  =  (usuarioEncontrado) ? true : false;  
             bandera ?  swal({
@@ -55,14 +62,14 @@ registro.addEventListener("click", (e) => {
     e.preventDefault();
     let userLogin = document.getElementById("user").value;
     let passLogin = document.getElementById("pass").value;
-
-    usuarios = JSON.parse(localStorage.getItem("usuarios"));
-
+    //Aplicacion de OR por si el ls esta vacio como explique en la linea 41/42
+    usuarios = JSON.parse(localStorage.getItem("usuarios")) || usuarios;
+    // Prueba operadores ternarios
     const permiso = (userLogin == "") ? true : false
     permiso ? console.log("No puso usuario") : console.log("Si puso usuario")
-    
+        //Utilizacion del metodo find para evitar re-registros de usuarios ya creados con el mismo nombre.
         if (usuarios.find(user => user.user === userLogin)) {
-            // alert("Usuario existente");**************************************************
+            // alert("Usuario existente");*********
             swal({
                 title: "Usuario existente",
                 text: "El nombre de usuario que usted ha ingresado ya esta en uso, si usted es el dueño de la cuenta inicie sesion!",
@@ -70,13 +77,13 @@ registro.addEventListener("click", (e) => {
                 button: "Continuar",
             })
 
-        }else if( userLogin != "" && passLogin != "") {
+        }else if( userLogin != "" && passLogin != "") {    //Con este else if evitamos que el usuario ponga en blanco las casillas del registro
             let nuevoUsuario = new Usuario (userLogin, passLogin);
             usuarios.push(nuevoUsuario);
             console.log(usuarios)
             let usuariosAlmacenados = JSON.stringify(usuarios);
             localStorage.setItem("usuarios", usuariosAlmacenados);
-            // alert("Te has registrado con exito!"); **************************************
+            // alert("Te has registrado con exito!"); **********
             swal({
                 title: "Bien hecho!",
                 text: "Te has registrado con éxito!",
@@ -101,13 +108,15 @@ const log = position => {
 }
 
 const API_KEY = 'ebe9a7d6692142e664e6d16014cc1adc';
-
+// Uso de fetch
 const fetchData = position => {
     const { latitude, longitude} = position.coords;
     fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
         .then(response => response.json())
         .then(data => setWeatherData(data))
     }
+
+// Manejo de los datos de la api.    
 const setWeatherData = data =>{
     console.log(data)
     const weatherData = {
@@ -121,25 +130,16 @@ const setWeatherData = data =>{
         document.getElementById(key).textContent = weatherData[key]
     });
 }
+//Setear fecha
 const getDate = () => {
     let date = new Date();
     return `${date.getDate()}-${ ('0' + (date.getMonth()+ 1)).slice(-2)}-${date.getFullYear()}`;
 }
 
+//Geolocalizador de usuario
 const onLoad = () => {
     navigator.geolocation.getCurrentPosition(fetchData);
 }
 
-    
-// //Prueba desctructuring
-// const {user, pass} = usuario1
-// console.log(user)
-// console.log(pass)
-
-// //Prueba de spread
-// const datos = {
-//     ...usuario1
-// }
-// console.log(datos)
 
 
